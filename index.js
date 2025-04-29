0e26b9d1bbe864585c0f2ca7c3c4cee271aa824d
sdefrancesco <skaterbastian@aol.com>
2025-04-29 09:41:58 -0400

    add post route

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json());
+
+
 // import models
 const Post = require('./models/post.js')
 
+// routes
 app.get('/api/posts', async (req, res) => {
-    
     try {
         const posts = await Post.find()
 
         res.json(posts)
+    } catch(err) {
+        console.log(err)
+        res.send(err.message)
+    }
+})
+app.post('/api/posts', async (req, res) => {
+    try {
+        let newPost = new Post(req.body)
+        let savedPost = await newPost.save()
+        res.json(savedPost)
     } catch(err) {
         console.log(err)
         res.send(err.message)
